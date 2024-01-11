c5c10d64474a4f8c6732af5739549789f61b92a6
Muhammad Junaid Iqbal <junaidi2k1@gmail.com>
2024-01-11 16:09:02 +0500

    - feat(telescope): add file_browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,10 +8,12 @@ return {
     "nvim-lua/popup.nvim",
     "nvim-telescope/telescope-media-files.nvim",
     "debugloop/telescope-undo.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
 
     telescope.setup({
       defaults = {
@@ -29,10 +31,48 @@ return {
           },
         },
       },
+      extensions = {
+        file_browser = {
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+          wrap_results = true,
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          winblend = 0,
+          mappings = {
+            -- your custom insert mode mappings
+            ["n"] = {
+              -- your custom normal mode mappings
+              ["N"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["l"] = fb_actions.toggle_browser,
+              [";"] = fb_actions.goto_cwd,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["<C-u>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-d>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+              ["<C-b>"] = actions.preview_scrolling_up,
+              ["<C-f>"] = actions.preview_scrolling_down,
+            },
+          },
+        },
+      },
     })
+
     telescope.load_extension("media_files")
     telescope.load_extension("fzf")
     telescope.load_extension("undo")
+    telescope.load_extension("file_browser")
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
@@ -41,7 +81,9 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 
-    keymap.set("n", "<leader>fp", ":Telescope projects", { desc = "Open recent projects" })
+    keymap.set("n", "<leader><leader>", "<cmd>Telescope file_browser<cr>", { desc = "File Editor Telescope" })
+
+    -- keymap.set("n", "<leader>fp", ":Telescope projects", { desc = "Open recent projects" })
     keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>", { desc = "Undo Tree" })
   end,
 }
