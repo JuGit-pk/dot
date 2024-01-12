c7283e07e40d43b7be8837c12428ff9fed7e3715
Muhammad Junaid Iqbal <junaidi2k1@gmail.com>
2024-01-12 23:38:36 +0500

    - refactor(keymaps): cleanup unused  keymaps and add some comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,37 +1,27 @@
--- local discipline = require("craftzdog.discipline")
--- discipline.cowboy()
-
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- use jk to exit insert mode
+-- Exit n mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
+-- Cut character without yanking
 keymap.set("n", "x", '"_x')
 
 -- Increment/decrement
 -- keymap.set("n", "+", ":inc<cr>", opts)
 -- keymap.set("n", "-", ":dec<cr>", opts)
--- Delete a word backward
-keymap.set("n", "dw", 'vb"_d')
 
--- Move selected lines
--- Select all
+-- Move selected lines - inspired from the ThePrimeagen
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- keymap.set("n", "<C-a>", "gg<S-v>G")
-
--- Save with root permission (not working for now)
---vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- Disable continuations
 keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
 keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+-- keymap.set("n", "<C-m>", "<C-i>", opts)
 
--- New tab
+-- Tab Management
 keymap.set("n", "te", ":tabedit")
 keymap.set("n", "tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
@@ -46,27 +36,27 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "s-", ":split<Return>", opts)
 keymap.set("n", "s|", ":vsplit<Return>", opts)
--- Close window
 keymap.set("n", "sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+-- keymap.set("n", "sh", "<C-w>h")
+-- keymap.set("n", "sk", "<C-w>k")
+-- keymap.set("n", "sj", "<C-w>j")
+-- keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set("n", "<leader>h", "<C-w><")
-keymap.set("n", "<leader>l", "<C-w>>")
-keymap.set("n", "<leader>j", "<C-w>+")
-keymap.set("n", "<leader>h", "<C-w>-")
+-- keymap.set("n", "<leader>h", "<C-w><")
+-- keymap.set("n", "<leader>l", "<C-w>>")
+-- keymap.set("n", "<leader>j", "<C-w>+")
+-- keymap.set("n", "<leader>h", "<C-w>-")
 
 -- keymap.set("n", "<C-w><left>", "<C-w><")
 -- keymap.set("n", "<C-w><right>", "<C-w>>")
 -- keymap.set("n", "<C-w><up>", "<C-w>+")
 -- keymap.set("n", "<C-w><down>", "<C-w>-")
--- -------
+
 -- PLUGINS
--- -------
+
 -- Undo Tree
 keymap.set("n", "<leader>uut", vim.cmd.UndotreeToggle, { desc = "Undo Tree" })
 --  IncRename
